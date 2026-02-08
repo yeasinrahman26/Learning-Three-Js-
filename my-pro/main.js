f3d723abdfd27a10b9a308b3ffe3107c2b285ef6
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2026-02-08 06:38:22 +0600

    learn how to use js Three in vanilajs and orbit control

---
+++ my-pro/main.js
@@ -0,0 +1,45 @@
+import "./index.css";
+
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const canvas = document.querySelector("canvas");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+  //   cube.rotation.x += 0.01;
+  cube.scale.z = 2;
+  cube.rotation.y += 0.01;
+  //   cube.rotation.z += 0.01;
+}
+animate();
